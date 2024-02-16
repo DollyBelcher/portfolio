f83608f5f02b0dac68a97c342f6599537253ecff
Dolly Belcher <dollybelcher@hotmail.com>
2024-02-16 08:53:55 +0000

    Developing UI and cleaning data.

---
+++ streamlit/pages/OMP Audience Planning Tool.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.header("OMP Audience Planning Tool")
+st.markdown("""- Developed a dashboard to give sales teams a comprehensive view of the top 50 advertisers spend in the open auction.
+- Provides details on advertisers' spending patterns and audience reach.
+- Integrates five distinct datasets from sources such as Google Ad Manager, Permutive, and Connect.
+- Aims to equip salespeople with first-party data for upselling direct advertising.
+- Addresses the growing importance of direct advertising due to the depreciation of third-party cookies.""")
+
+st.markdown("Revenue Analysis:")
+st.image(image="../docs/Picture1.png")
+
+
+st.markdown("Demographic Analysis:")
+st.image(image="../docs/OMP 2.png")
